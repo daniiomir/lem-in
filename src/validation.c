46175f68827e7d934c317e9001da55880d623425
swarner <daniiomir@yandex.ru>
2019-11-12 18:32:20 +0300

    to norminette

--- src/validation.c
+++ src/validation.c
@@ -44,7 +44,8 @@ void			add_map(char **map, char *str)
 	*map = ft_strjoin_free(*map, "\n");
 }
 
-static void		first_part_check(t_lem *lem, char *str, int *line_number, int ret)
+static void		first_part_check(t_lem *lem, char *str,
+	int *line_number, int ret)
 {
 	if (ret == -1)
 		error_exit(lem, 1);
@@ -52,13 +53,34 @@ static void		first_part_check(t_lem *lem, char *str, int *line_number, int ret)
 		*line_number = first_line(lem, str, *line_number);
 }
 
-static void		start_end_instructions(t_lem *lem, int *start, char *str, char **map)
+static void		start_end_instructions(t_lem *lem, int *start,
+	char *str, char **map)
 {
 	(*start)++;
 	if (*start > 1)
 		error_exit(lem, 1);
 	add_map(map, str);
 	free_str(&str);
+}
+
+static void		for_links(t_lem *lem, char *str, int *line_number, char **map)
+{
+	if (ft_strchr(str, '-') && str[0] != '#'
+		&& str[0] != 'L' && *line_number > 1)
+		add_link(lem, str);
+	(*line_number)++;
+	add_map(map, str);
+	if (str)
+		free(str);
+}
+
+void			fuck_this_shit_im_done(int *start, char **str,
+	int *end, int *line_number)
+{
+	*start = 0;
+	*str = NULL;
+	*end = 0;
+	*line_number = 0;
 }
 
 void			parse_map(t_lem *lem, int ret, int fd, char **map)
@@ -68,10 +90,7 @@ void			parse_map(t_lem *lem, int ret, int fd, char **map)
 	int		line_number;
 	char	*str;
 
-	start = 0;
-	str = NULL;
-	end = 0;
-	line_number = 0;
+	fuck_this_shit_im_done(&start, &str, &end, &line_number);
 	while ((ret = get_next_line(fd, &str)))
 	{
 		first_part_check(lem, str, &line_number, ret);
@@ -88,12 +107,7 @@ void			parse_map(t_lem *lem, int ret, int fd, char **map)
 		if (!ft_strchr(str, '-') && str[0] != '#'
 		&& str[0] != 'L' && line_number > 1)
 			fill_rooms(lem, &str, &start, &end);
-		if (ft_strchr(str, '-') && str[0] != '#'
-		&& str[0] != 'L' && line_number > 1)
-			add_link(lem, str);
-		line_number++;
-		add_map(map, str);
-		free_str(&str);
+		for_links(lem, str, &line_number, map);
 	}
 	start_end_errors(lem, start, end);
 }
